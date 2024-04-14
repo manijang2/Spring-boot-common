374a9da8321e2540ef7a25c4405278eb9d09263a
manijang2 <manijang2@naver.com>
2024-04-14 19:48:16 +0900

    init

---
+++ src/main/java/manijang2/common/SpringBootCommonApplication.java
@@ -0,0 +1,13 @@
+package manijang2.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/manijang2/common/SpringBootCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package manijang2.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
